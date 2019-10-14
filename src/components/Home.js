2047f182b00bc38fad6bad1fe6a9058c10a8cbe1
Tommy Clark <tommy.clark@sainsburys.co.uk>
2019-10-14 16:54:43 +0100

    Add "Loading..." to splash screen until the Heroku app wakes up.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import logo from "../logo.svg";
 import {Link} from "react-router-dom";
+import axios from "axios";
+import * as Constants from "../constants";
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { loadingFinished: false };
+
+
+        axios.get(`${Constants.default.BASE_URL}/health`)
+            .then(response => {
+                this.setState({loadingFinished: true});
+                this.forceUpdate();
+            });
+    }
+
     render() {
+        const loadingFinished = this.state.loadingFinished;
+
+        let loadingBar;
+        if (loadingFinished) {
+            loadingBar = <Link to={'/team'} className="nav-link">Play the game</Link>;
+        } else {
+            loadingBar = "Loading...";
+        }
+
         return (
             <div className="App-body">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    <Link to={'/team'} className="nav-link">Play the game</Link>
+                    {loadingBar}
                 </p>
             </div>
         );
